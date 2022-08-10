b12f24cfbf338c228951233ab54190a99c74f03c
Laplace <vigooktarioa@gmail.com>
2022-08-10 12:17:23 +0700

    done?

---
+++ src/components/todo.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+
+export default function Todo() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+      if(localStorage.getItem("localTasks")){
+          const storedList = JSON.parse(localStorage.getItem("localTasks"));
+          setTasks(storedList);
+      }
+  },[])
+
+  const addTask = (e) => {
+    if (task) {
+      const newTask = { id: new Date().getTime().toString(), title: task };
+      setTasks([...tasks, newTask]);
+      localStorage.setItem("localTasks", JSON.stringify([...tasks, newTask]));
+      setTask("");
+    }
+  };
+
+  const handleDelete = (task)=>{
+      const deleted = tasks.filter((t)=>t.id !== task.id);
+      setTasks(deleted);
+      localStorage.setItem("localTasks", JSON.stringify(deleted))
+  }
+
+  const handleClear=()=>{
+      setTasks([]);
+      localStorage.removeItem("localTasks");
+  }
+  return (
+    <div className="container row">
+      <h1 className="mt-3 text-black mb-5">To-Do App</h1>
+      <div className="col-4 mb-3">
+        <input
+          name="task"
+          type="text"
+          value={task}
+          placeholder="Write your task..."
+          className="form-control"
+          onChange={(e) => setTask(e.target.value)}
+        />
+      </div>
+      <div className="col-4">
+        <button
+          className="btn btn-primary form-control material-icons"
+          onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      {tasks.map((task) => (
+        <React.Fragment key={task.id}>
+            <div className="col-11">
+                <span className = "form-control bg-white btn mt-2 border border-primary"
+                style={{textAlign: "left", fontWeight: "bold"}}>
+                    {task.title}
+                </span>
+            </div>
+
+            <div className="col-1">
+                <button
+                className =" mt-2 btn btn-warning material-icons"
+                onClick ={()=> handleDelete(task)}
+                >delete</button>
+            </div>
+        </React.Fragment>
+      ))}
+      {!tasks.length ? null:(
+          <div>
+              <button className= "btn btn-secondary  mt-4 mb-4" onClick={()=>handleClear()}>
+                  Clear
+              </button>
+          </div>
+      )}
+    </div>
+  );
+}
